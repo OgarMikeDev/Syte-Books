84b5918f2d6ebe46e2716ef2bf6a403aa5757bbe
Mike Ogar <ogar.m.v@mail.ru>
2023-01-21 09:46:28 +0300

    "Spring-Syte"

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,9 +3,9 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Class Main
 @SpringBootApplication
 public class Main {
+    //Method main
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
